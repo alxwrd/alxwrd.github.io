83da3a5a58de24ccf3ca9b3aae987d4c3f472482
Alex Ward <alxwrd@googlemail.com>
2024-02-17 16:23:57 +0000

    add pagetitle component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://alxwrd.co.uk/",
     integrations: [
         tailwind(),
+        icon(),
     ],
 });
