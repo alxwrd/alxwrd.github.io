6d270f72941083981ba68bc426f556e8d60f7e1b
Alex Ward <alxwrd@googlemail.com>
2024-03-06 21:03:52 +0000

    remove trailing slash from Astro.site.href

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://alxwrd.co.uk/",
+    site: "https://alxwrd.co.uk",
     integrations: [
         tailwind(),
         icon(),
